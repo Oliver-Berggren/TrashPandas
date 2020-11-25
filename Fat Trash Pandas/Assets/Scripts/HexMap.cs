adffdf9d18b1b4cd72e73242dcbf51d07b0d042b
coagulantLegume <alvwolf@ucsc.edu>
2020-11-24 21:01:05 -0800

    Map from file implemented, need to fix mirroring. worldToHex and hexToWorld untested.

--- Fat Trash Pandas/Assets/Scripts/HexMap.cs
+++ Fat Trash Pandas/Assets/Scripts/HexMap.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class HexMap : MonoBehaviour
 {
@@ -8,9 +9,7 @@ public class HexMap : MonoBehaviour
     public GameObject tile;
     float sideLength;
 
-    // Dimensions
-    public int width;
-    public int height;
+    // Display Parameters
     public float spacing;
 
     // Placement unit values
@@ -20,8 +19,12 @@ public class HexMap : MonoBehaviour
     // Map Container
     Dictionary<Vector2, GameObject> tileMap = new Dictionary<Vector2, GameObject>();
 
+    // Map layout in text file form
+    public TextAsset mapLayout;
+
     void Start()
     {
+        List<List<int>> mapRaw = textToLayout();
         // Calculate length of one side(same as distance from center point to corner point)
         sideLength = tile.GetComponent<Renderer>().bounds.size.x / 2;
 
@@ -30,16 +33,28 @@ public class HexMap : MonoBehaviour
         xOffset = Mathf.Sqrt(3) * (sideLength + spacing);
 
         // Spawn Map tiles
-        for(int y = 0; y < height; ++y)
+        for(int y = 0; y < mapRaw.Count; ++y)
         {
             float lineOffset = (y % 2) * (xOffset / 2);
-            for(int x = 0; x < width; ++x)
+            for(int x = 0; x < mapRaw[y].Count; ++x)
             {
-                GameObject newTile = Instantiate(tile, new Vector3(x * xOffset + lineOffset, 0, y * yOffset), Quaternion.identity);
-                newTile.transform.parent = this.transform;
-                newTile.name = "( " + x + " , " + y + " )";
-                newTile.GetComponent<TileInfo>().init(-1, null, new Vector2(x,y));
-                tileMap[new Vector2(x,y)] = newTile;
+                // none: 0, empty: 1, dump: 2, den: 3, gas: 4, hole: 5, trash: 6
+                if(mapRaw[y][x] != 0) // tile drawn
+                {
+                    GameObject newTile = Instantiate(tile, new Vector3(x * xOffset + lineOffset, 0, y * yOffset), Quaternion.identity);
+                    newTile.transform.parent = this.transform;
+                    newTile.name = "( " + x + " , " + y + " )";
+                    tileMap[new Vector2(x,y)] = newTile;
+
+                    if(mapRaw[y][x] == 6) // empty tile with trash placed
+                    {
+                        newTile.GetComponent<TileInfo>().init(6, null, new Vector2(x,y));
+                    }
+                    else // static or empty tile
+                    {
+                        newTile.GetComponent<TileInfo>().init(mapRaw[y][x], null, new Vector2(x,y));
+                    }
+                } // otherwise empty space with no tile
             }
         }
     }
@@ -59,8 +74,29 @@ public class HexMap : MonoBehaviour
         return new Vector3(hexPos.x * xOffset + (hexPos.y % 2) * (xOffset / 2), 0, hexPos.y * yOffset);
     }
 
-    // public List<TileInfo> getNeighbors(Vector2 tile)
-    // {
-// 
-    // }
+    // Makes 2d list of map with tile types from text file
+    List<List<int>> textToLayout()
+    {
+        List<List<int>> map = new List<List<int>>();
+        map.Add(new List<int>());
+
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < mapLayout.text.Length; ++i)
+        {
+            int tileValue = mapLayout.text[i] - '0';
+            if(tileValue >= 0 && tileValue <= 6)
+            {
+                ++x;
+                map[y].Add(tileValue);
+            }
+            else if(mapLayout.text[i] == '\n')
+            {
+                ++y;
+                x = 0;
+                map.Add(new List<int>());
+            }
+        }
+        return map;
+    }
 }
