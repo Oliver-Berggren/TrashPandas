9ad9bdfa6887d5bb04ca0cc760e6ea4f323a3fa3
Stryker Buffington <strykerbuffington@gmail.com>
2020-12-10 12:59:58 -0800

    rules subscreen added

---
+++ Fat Trash Pandas/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainmenucomponents;
+    public GameObject rulescomponents;
+
+    private void Start()
+    {
+        //play.enabled = false;
+    }
+
+    public void ShowRules()
+    {
+        rulescomponents.gameObject.SetActive(true);
+        mainmenucomponents.gameObject.SetActive(false);
+    }
+
+    public void HideRules()
+    {
+        rulescomponents.gameObject.SetActive(false);
+        mainmenucomponents.gameObject.SetActive(true);
+    }
+
+}
